a6d108c248b4579bd92c6046af677d4caf30c967
Tovah Parnes <tovah@parnes.com>
2022-10-02 14:16:47 +0200

    functioning list but without scroll

--- task2/lib/rightPage.dart
+++ task2/lib/rightPage.dart
@@ -2,14 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'homePage.dart';
 
-class RightPage extends StatefulWidget {
-  const RightPage({super.key});
+class RightPage extends StatelessWidget {
+  const RightPage({Key? key}) : super(key: key);
 
   @override
-  State<RightPage> createState() => _RightPageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        
+        body: const Center(
+          child: RandomWords(),
+        ),
+      )
+    );
+  }
 }
 
-class _RightPageState extends State<RightPage> {
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
@@ -21,7 +39,7 @@ class _RightPageState extends State<RightPage> {
 
         final index = i ~/ 2; /*3*/
         if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(20)); /*4*/
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
         return ListTile(
           title: Text(
